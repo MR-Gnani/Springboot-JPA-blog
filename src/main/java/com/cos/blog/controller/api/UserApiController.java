718e2f7f574f5a35a20f3cd36d898edb73222036
MR NANI <nani@MRui-MacBookAir.local>
2023-10-23 17:43:14 +0900

    시큐리티셋팅

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -19,9 +19,6 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	@Autowired
-	private HttpSession session;
-	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) { //username, password, email
 		System.out.println("UserApiController : save 호출됨");
@@ -31,8 +28,9 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
 	}
 	
+	/*
 	@PostMapping("api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user) {
+	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
 		System.out.println("UserApiController : save 호출됨");
 		User principal = userService.로그인(user); // principal(접근주체)
 		
@@ -40,6 +38,6 @@ public class UserApiController {
 			session.setAttribute("principal", principal);
 		}
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-	}
+	} */
 	
 }
